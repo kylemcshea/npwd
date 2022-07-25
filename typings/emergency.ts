8057c2fed05bc54c46c4ce2fe585288d77ba6a5b
Kyle McShea <46171436+kylemcshea@users.noreply.github.com>
2022-07-25 17:16:53 -0400

    refactor(911): cleaning typing

--- typings/emergency.ts
+++ typings/emergency.ts
@@ -27,6 +27,3 @@ export interface DispatchModel {
 export interface AnimationModel {
   isCalling: bool;
 }
-
-// addEventHandler("core_dispatch:addCall", function(code, title, info, location, job, cooldown, sprite, color)
-//    TriggerServerEvent("core_dispatch:addCall","10-90","Bank Robbery in Progress",{{icon = "fa-university", info = ''}},{LD.FleecaBanks[name].doors.startloc.x, LD.FleecaBanks[name].doors.startloc.y, LD.FleecaBanks[name].doors.startloc.z},"police",5000,375,21)
